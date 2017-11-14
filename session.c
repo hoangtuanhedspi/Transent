c8c50be387a237e54657a2a21ae2facb40f544ac
haihv <haihv27@gmail.com>
2017-11-14 23:44:37 +0700

    Session

---
+++ session.c
@@ -0,0 +1,59 @@
+#include "session.h"
+
+Session *findSessionByConnfd (int connfd) {
+    int i;
+    for (i = 0; i < SESSIONS; i++) {
+        if (sessions[i].connfd == connfd) {
+            return sessions + i;
+        }
+    }
+
+    return NULL;
+}
+
+_Bool newSession (struct sockaddr_in *cliaddr, int connfd) {
+    int i;
+    for (i = 0; i < SESSIONS; i++) {
+        if (sessions[i].cliaddr == NULL) {
+            sessions[i].cliaddr = cliaddr;
+            sessions[i].connfd = connfd;
+
+            return 1;
+        }
+    }
+
+    if (i == SESSIONS) {
+        return 0;
+    } 
+}
+
+_Bool removeSession (Session *ss) {
+    for (int i = 0; i < SESSIONS; i++) {
+        if (isSameSession(sessions + i, ss)) {
+            sessions[i].cliaddr = NULL;
+            sessions[i].connfd = -1;
+            
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+_Bool isSameSession (Session *ss1, Session *ss2) {
+    if (isEqualSockAddrIn(ss1->cliaddr, ss2->cliaddr) && ss1->connfd == ss2->connfd)
+        return 1;
+    else
+        return 0; 
+}
+
+void initSessions () {
+    for (int i = 0; i < SESSIONS; i++) {
+        sessions[i].cliaddr = NULL;
+        sessions[i].connfd = -1;
+    }
+}
+
+_Bool isEqualSockAddrIn (struct sockaddr_in *addr1, struct sockaddr_in *addr2) {
+    return (addr1->sin_addr.s_addr == addr2->sin_addr.s_addr && addr1->sin_port == addr2->sin_port);
+}
---
+++ session.h
@@ -0,0 +1,27 @@
+#ifndef SESSION_H
+#define SESSION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <netinet/in.h>
+
+#define SESSIONS 5
+
+typedef struct Session_ {
+    struct sockaddr_in *cliaddr;
+    int connfd;
+} Session;
+
+extern Session sessions[SESSIONS];
+extern int no_sessions;
+
+
+Session *findSessionByConnfd (int connfd);
+_Bool newSession (struct sockaddr_in *cliaddr, int connfd);
+_Bool removeSession (Session *ss);
+_Bool isSameSession (Session *ss1, Session *ss2);
+void initSessions ();
+_Bool isEqualSockAddrIn (struct sockaddr_in *addr1, struct sockaddr_in *addr2);
+
+#endif
