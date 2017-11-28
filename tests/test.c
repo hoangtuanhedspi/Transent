4616bc1f9ce79445acd6232572ab3ba83ba6b3fb
Hoang Tuan <hoangtuanictvn@gmail.com>
2017-11-28 16:43:42 +0700

    Update test case

--- tests/test.c
+++ tests/test.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <transent/util.h>
 #include <poll.h>
+#include <assert.h>
+#include<string.h>
+#define DEBUG 1
+#include <transent/util.h>
+#include <transent/interface.h>
 
+void test_interface(int argc, char* argv[]);
 int main(int argc, char* argv[]){
-    printf("%d\n",get_user_method(1));
+    test_interface(argc,argv);
     return 0;
+}
+
+void test_interface(int argc, char* argv[]){
+    char buff[BUFF_SIZE];
+    printf("==============Test interface===============\n");
+    add_request(buff,1);
+    int i = extract_request(buff);
+    attach_payload(buff,"Hello",3);
+    char* pl = detach_payload(buff);
+    loginfo("\nPackage contain\nmethod:%d at pack %d \npayload:%s\npayload_size:%dbyte\n",i,pack(1),pl,get_payload_size(buff));
+    assert(i==1);
+    assert(strcmp(pl,"Hel")==0);
 }
