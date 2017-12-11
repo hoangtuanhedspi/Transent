f3df3bb4e73ee1bc59bb5c8921c7a90428e661c3
Hoang Tuan <hoangtuanictvn@gmail.com>
2017-12-11 16:30:18 +0700

    Add cache function

--- src/tsfmanage.c
+++ src/tsfmanage.c
@@ -1,26 +1,69 @@
-#include "../include/session.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "../include/util.h"
-#include "../include/slist.h"
 #include "../include/tsfmanage.h"
 
-int init_cache_context(){
-    return -1;
+int is_init = 0;
+int saved_cache_size = 0;
+
+#define throw_init_error if(!is_init){\
+    printf("Please initial cache context\
+             before using it!\n");\
+    exit(0);\
 }
 
-TSFileCache * new_cache_file(Session* session,char* file_name){
-    TSFileCache* cache = tsalloc(TSFileCache,1);
-    cache->session = NULL;
-    if(!cache)
-        return NULL;
-    cache->file_name = file_name;
-    printf("Pass!\n");
-    cache->session = copy_session(session);
-    return cache;
+int init_cache_context(CacheList** cache_list){
+    if (!is_init)
+    {
+        is_init = 1;
+        *cache_list = tsalloc(Node,1);
+        (*cache_list)->data = NULL;
+    }
+    return 1;
 }
 
-int add_file_to_cache(Node** list,TSFileCache *file_cache){
-    printf("Pass!\n");
-    if(append(list,file_cache)!=NULL) 
-        return 1;
-    return -1;
+Cache * new_cache(char* file_name,char* uid_hash){
+    throw_init_error
+    if(!file_name || !uid_hash) return NULL;
+    Cache *res = tsalloc(Cache,1);
+    if(!res) return NULL;
+    strcpy(res->file_name,file_name);
+    strcpy(res->uid_hash,uid_hash);
+    return res;
+}
+
+Cache* push_cache(CacheList * list,Cache* cache){
+    throw_init_error
+    if(!list) return NULL;
+    saved_cache_size ++;
+    return n_append(list,cache)->data;
+}
+
+int get_cache_size(CacheList *list){
+    throw_init_error
+    if(!list) return 0;
+    return length(list);
+}
+
+int get_all_cache_size(){
+    return saved_cache_size;
+}
+
+int compare_cache(Var des,Var res){
+    return strcmp(((Cache*)des)->file_name,((Cache*)res)->file_name) == 0 
+           & strcmp(((Cache*)des)->uid_hash,((Cache*)res)->uid_hash) == 0;
+}
+
+int remove_cache(CacheList* list,Cache* cache){
+    --saved_cache_size;
+    if(list->next==NULL){
+        list->data = NULL;
+        return 0;
+    }
+    return remove_data(&list,compare_cache,cache);
+}
+
+int cache_contain(CacheList * list,Cache * cache){
+    return find_data(list,compare_cache,cache) != NULL;
 }
