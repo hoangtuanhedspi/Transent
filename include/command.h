2923c1196fd1dedbd82ef38cd0be4fb7c71a9297
Hoang Tuan <hoangtuanictvn@gmail.com>
2017-12-31 12:08:09 +0700

    Update command parser

--- include/command.h
+++ include/command.h
@@ -1,1 +1,17 @@
 #ifndef _TRANSENT_COMMAND_
+#define _TRANSENT_COMMAND_
+
+#define METH_LEN 10
+#define DATA_LEN 200
+
+typedef struct _cmd{
+    char str_cmd[METH_LEN+DATA_LEN];
+    char method[METH_LEN];
+    char data[DATA_LEN];
+}Command;
+
+int valid_str_cmd(char* args);
+Command* parse_cmd(char* args);
+int valid_cmd(Command cmd);
+Command* suggest_cmd(Command cmd);
+#endif
