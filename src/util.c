c92bd01d3320a64db2f34d5cbd773051ecc1c107
Hoang Tuan <hoangtuanictvn@gmail.com>
2017-12-31 12:08:55 +0700

    Refactor util

--- src/util.c
+++ src/util.c
@@ -6,27 +6,6 @@
 #include "../include/slist.h"
 #include "../include/session.h"
 
-MenuAction get_user_method(int logedin){
-    int ret = -1;
-    printf("1.Send file\n2.Logout\nSelect:");
-    scanf("%d",&ret);
-    while(getchar()!='\n');
-    if(ret==1) ret = SENDFILE;
-    else if(ret==2) ret = LOGOUT;
-    else ret = UNKNOWN;
-    return valid_action(ret);
-}
-
-MenuAction valid_action(MenuAction action){
-    switch(action){
-        case SENDFILE:
-        case LOGIN:
-        case LOGOUT: return action;
-        default: return UNKNOWN;
-    }
-    return UNKNOWN;
-}
-
 int parse_packet(char* buff,char* payload,int* size){
     int req_method = UNDEFINE;
     req_method = extract_request(buff);
